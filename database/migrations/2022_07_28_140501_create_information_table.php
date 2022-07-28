eebfab30e4928eb26e5ec1988fb43d0c90d86e5e
ACER <tlemisovilham001@gmail.com>
2022-07-28 19:37:56 +0500

    Shaygon

--- database/migrations/2022_07_28_140501_create_information_table.php
+++ database/migrations/2022_07_28_140501_create_information_table.php
@@ -15,8 +15,8 @@ class CreateInformationTable extends Migration
     {
         Schema::create('information', function (Blueprint $table) {
             $table->id();
-            $table->message('string');
-            $table->file('file');
+            $table->string('message');
+            $table->string('file');
             $table->timestamps();
         });
     }
